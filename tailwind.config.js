bdc6e03e94a9c8006abbf07821ff3c7109c20ae6
rinchjo <83864991+rinchjo@users.noreply.github.com>
2022-01-05 20:41:04 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        'bImage': "url(http://amjiltacademy.com/wp-content/uploads/2021/10/dark8.jpg)",
+      },
+    },
+  },
+  plugins: [],
+};
